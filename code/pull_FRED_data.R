e5bf5bf03b6c4fe4184746132da57f50da257bef
matdehaven <matthew_dehaven@brown.edu>
2022-12-04 18:09:27 -0500

    Added vacancy & tightness data

--- code/pull_FRED_data.R
+++ code/pull_FRED_data.R
@@ -19,14 +19,32 @@ fredr::fredr_set_key(apiKey)
 ## Pull FRED Series
 series <- c(
   u = "LNS14000061", # UNRATE is overall, using prime age, male UR to match paper
-  i = "CPIAUCSL", # Could use GDPCTPI (GDP Deflator)
-  ff = "FEDFUNDS"
+  pi = "CPIAUCSL", # Could use GDPCTPI (GDP Deflator)
+  ff = "FEDFUNDS",  
+  vlev = "JTSJOL",  # Nonfarm vacancies in number
+  lflev = "CLF16OV" # Labor force in number
 )
 data <- rbindlist(lapply(series, fredr::fredr))[,.(date, series_id, value)] 
 data <- data |> dcast(date ~ series_id)
 order <- c("date", series)
 data <- data[,..order]
 names(data) <- c("date", names(series))
+data[, v := 100 * vlev / lflev]
+data[, vlev := NULL][, lflev := NULL]
+
+## Read in Historical Vacancy & Urate data (from Pascal)
+ebc_data <- readxl::read_excel("./data/EBC_data.xlsx", sheet = 2, skip = 1) |> as.data.table()
+names(ebc_data) <- c('year', 'month', 'urate', 'laborforce', 'vaclevel', 'vrateB', 'uratePNZ', 'vratePNZ')
+ebc_data[, date := as.Date(paste0(year, "-", month, "-01"))]
+ebc_data[, v_hist := fifelse(is.na(vratePNZ), 100*vaclevel / laborforce, vratePNZ)]
+
+## Merge onto data
+data <- merge(data, ebc_data[!is.na(v_hist),.(date, v_hist)], by = "date", all.x = T)
+data[is.na(v), v := v_hist]
+data[, v_hist := NULL]
+
+## Calculate Tightness (simply Theta = V/U)
+data[, theta := v / u]
 
 ## Read in Shadow Rate data
 shadow <- readxl::read_excel("./data/WuXiaShadowRate.xlsx", sheet = 2) |> as.data.table()
@@ -42,18 +60,18 @@ data <- data |> melt(id = "date", variable.name = "series_id")
 
 ## Seasonally Adjust
 
-seasonally_adjust <- function(x, dates){
-  
-  ts_x <- as.ts(zoo::zoo(x, zoo::as.yearmon(dates), frequency = 12, calendar = "yearmon"))
-  
-  ts_x_adj <- seasonal::seas(ts_x, x11 = "", na.action = na.exclude)
-  
-  return(seasonal::final(ts_x_adj))
-}
-
-seasonally_adjust(data[series_id=="u", value], data[series_id=="u", date])
-
-data[, value_adj := seasonally_adjust(value, date), by = .(series_id) ]
+# seasonally_adjust <- function(x, dates){
+#   
+#   ts_x <- as.ts(zoo::zoo(x, zoo::as.yearmon(dates), frequency = 12, calendar = "yearmon"))
+#   
+#   ts_x_adj <- seasonal::seas(ts_x, x11 = "", na.action = na.exclude)
+#   
+#   return(seasonal::final(ts_x_adj))
+# }
+# 
+# seasonally_adjust(data[series_id=="u", value], data[series_id=="u", date])
+# 
+# data[, value_adj := seasonally_adjust(value, date), by = .(series_id) ]
 
 ## Plot the series to inspect
 data |>
@@ -61,7 +79,7 @@ data |>
     x = date
   )) +
   geom_line(aes(y = value, color = "raw")) + 
-  geom_line(aes(y = value_adj, color = "seasonal")) + 
+  #geom_line(aes(y = value_adj, color = "seasonal")) + 
   facet_wrap(vars(series_id), scale = "free_y")
 
 ## Small difference from seasonal adjustment
